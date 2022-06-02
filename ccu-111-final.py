3d36f8b6471e4acdb9c3e949f84a1f04ba5834e8
slug001 <84364313+slug001@users.noreply.github.com>
2022-06-02 20:47:15 +0800

    Update ccu-111-final.py

--- ccu-111-final.py
+++ ccu-111-final.py
@@ -1,12 +1,19 @@
-from flask import Flask, request,render_template
+from flask import Flask, request,render_template,session
 import requests
 import json
 import psycopg2
 import math
+import configparser
+
 database_url='postgres://zlujzrtgrbvpgb:ba830f737f30a8309677d917becefb9295f1956a8521b478604e3e4e54c76bb1@ec2-54-165-90-230.compute-1.amazonaws.com:5432/d64qh26kv7tm2k'
 
-app = Flask(__name__)
+#啟用config
+config = configparser.ConfigParser()
+config.read('config.ini')
 
+#設定flask 密碼、金鑰 
+app = Flask(__name__)
+app.config['SECRET_KEY']='ben9709830018'
 
 @app.route('/', methods=['GET','POST'])
 def home():

98c6ea8fceee7ac641b5fb3d4532f107687e56db
slug001 <84364313+slug001@users.noreply.github.com>
2022-06-02 15:29:10 +0800

    Update ccu-111-final.py

--- ccu-111-final.py
+++ ccu-111-final.py
@@ -18,7 +18,7 @@ def home():
         all_data=cursor.fetchall()
         cursor.close()
         conn.close()
-        all_data=json.dumps(all_data,encoding=”utf-8”)
+        all_data=json.dumps(all_data,encoding='utf-8')
         return render_template("home.html",repeat=all_data)
     else:
         #user_position=request.values['input']

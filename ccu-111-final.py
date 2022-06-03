d677a3b932746a5c30c1fb76629c5f71069898e7
slug001 <84364313+slug001@users.noreply.github.com>
2022-06-03 18:30:40 +0800

    Update ccu-111-final.py

--- ccu-111-final.py
+++ ccu-111-final.py
@@ -29,7 +29,7 @@ def home():
         conn.close()
         
         #all_data=json.dumps(all_data)
-        all_data=list(all_data[0])
+        all_data=[i for i in all_data]
         #all_data=all_data[0]
         
         """

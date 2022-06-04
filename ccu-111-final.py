d9491633c0c72f54162bb11785fcb224cca66c8a
slug001 <84364313+slug001@users.noreply.github.com>
2022-06-04 16:56:11 +0800

    Update ccu-111-final.py

--- ccu-111-final.py
+++ ccu-111-final.py
@@ -357,7 +357,7 @@ def handle_message(event):
         pass
     #位置資訊則進入第一個if
     if(event.message.type == 'location'):
-        tmp=event.source
+        tmp=event.source.type
         message = TextSendMessage(text = tmp)
         line_bot_api.reply_message(event.reply_token, message)
         

ade6b18010ef425d4e242fd2cf8ca2a40c3bc3a7
slug001 <84364313+slug001@users.noreply.github.com>
2022-06-04 21:31:19 +0800

    Update ccu-111-final.py

--- ccu-111-final.py
+++ ccu-111-final.py
@@ -452,7 +452,7 @@ def handle_message(event):
         cursor.close()
         conn.close()
         
-        if (tmp in all_user_name:
+        if (tmp in all_user_name):
             tt='success
         else:
             tt='false'

a1dadb09b52675328b30cb2974d3eb3dcb0fa699
slug001 <84364313+slug001@users.noreply.github.com>
2022-06-05 22:09:40 +0800

    Update ccu-111-final.py

--- ccu-111-final.py
+++ ccu-111-final.py
@@ -382,7 +382,7 @@ def handle_message(event):
         cursor.close()
         conn.close()
 
-        message = TextSendMessage(text = str(favorite))
+        message = TextSendMessage(text = favorite[0])
         line_bot_api.reply_message(event.reply_token, message)
         
         lat,lng = message_location(event)

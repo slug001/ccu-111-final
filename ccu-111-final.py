3ca65ba0cd24f3d056575cdee4910ce4bda7c07b
slug001 <84364313+slug001@users.noreply.github.com>
2022-06-05 23:01:19 +0800

    Update ccu-111-final.py

--- ccu-111-final.py
+++ ccu-111-final.py
@@ -388,8 +388,8 @@ def handle_message(event):
         lat,lng = message_location(event)
         #ap = "經度:{lat},緯度:{lng}".format(lat=lat,lng=lng)
         #尋找附近的店家
-        url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude}, {longitude}&radius=2000&keyword={keyword}&language=zh-TW&key=AIzaSyCiDz6zKepKyIrKlfFeYYagsapLT1Xa7qw"\
-            .format(latitude=lat,longitude=lng,keyword='麻辣')
+        url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude}, {longitude}&radius=2000&keyword='{keyword}'&language=zh-TW&key=AIzaSyCiDz6zKepKyIrKlfFeYYagsapLT1Xa7qw"\
+            .format(latitude=lat,longitude=lng,keyword=favorite[0])
         payload={}
         headers = {}
         #尋找附近店家

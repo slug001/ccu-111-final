e27cc894a5e93207b2b30476ffe2a6b2978da638
slug001 <84364313+slug001@users.noreply.github.com>
2022-06-06 23:59:04 +0800

    Update ccu-111-final.py

--- ccu-111-final.py
+++ ccu-111-final.py
@@ -397,6 +397,7 @@ def recommend():
                 data_web[i]="https://maps.googleapis.com/maps/api/place/photo?maxwidth=300&maxheight=250&photo_reference={photo_id}&key=AIzaSyBx2V_QiQ5aXZlV5RxvPOUqC90B511Kv0A".format(photo_id=data_web[i])
         data_web[4]='營業中' if data_web[4]== True else '休息中'
         data_for_web.extend(data_web)
+    data_for_web=[str(i) for i in data_for_web]
     data_for_web_str=",".join(data_for_web)
     #for i in data_for_web:
     #    data_for_web_str+=str(i)
